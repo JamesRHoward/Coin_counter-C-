7869d519ca195b55d344dd70999e2c3b64b22630
James Howard and Loren Glenn <james+loren@>
2016-11-29 14:19:22 -0800

    Backend finished

---
+++ Objects/Change.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System;
+
+namespace Coins.Objects
+{
+  public class Change
+  {
+    string[] _coinNames = new string[] {"Quarter", "Dime", "Nickle", "Penny"};
+    int[] _coinValues = new int[] {25, 10, 5, 1};
+    private int _inputAmount;
+    private int _total;
+
+    public Change(int inputAmount)
+    {
+      _inputAmount = inputAmount;
+    }
+
+    //Getters setters
+    public string ComputeChange()
+    {
+      int numberOfQuarters = 0;
+      int numberOfDimes = 0;
+      int numberOfNickles = 0;
+      int numberOfPennies = 0;
+      for (int i = 0; i < _coinNames.Length; i ++)
+      {
+        while (_inputAmount >= _coinValues[i])
+        {
+          _inputAmount -= _coinValues[i];
+          if(i == 0)
+          {
+            numberOfQuarters++;
+          }
+          if(i == 1)
+          {
+            numberOfDimes++;
+          }
+          if(i == 2)
+          {
+            numberOfNickles++;
+          }
+          if(i == 3)
+          {
+            numberOfPennies++;
+          }
+        }
+      }
+      string result = ("Quarters: " + numberOfQuarters + " Dimes: " + numberOfDimes + " Nickles: " + numberOfNickles + " Pennies: " + numberOfPennies + ".");
+      return result;
+    }
+  }
+}
---
+++ Tests/ChangeTest.cs
@@ -0,0 +1,19 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using Coins.Objects;
+
+namespace CoinsTest
+{
+  public class ChangeTest
+  {
+    [Fact]
+    public void Coins_InputAmountTwentyFiveGetQuarterBack_True()
+    {
+      Change testChange = new Change(50);
+      string resultCoins = testChange.ComputeChange();
+      Console.WriteLine(resultCoins);
+      Assert.Equal("Quarters: 2 Dimes:  Nickles:  Pennies:  .", resultCoins);
+    }
+  }
+}
